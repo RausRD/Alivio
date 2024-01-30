82a3a088bd16e088592d435fd57418420988f6cd
Ruslan Dunets <dunecruslan2@gmail.com>
2024-01-30 13:21:15 +0200

    added video

--- js/main.js
+++ js/main.js
@@ -7,3 +7,30 @@ mobileNavBtn.addEventListener('click', function () {
 	mobileNav.classList.toggle('active')
 	document.body.classList.toggle('no-scroll')
 })
+
+// =======================video==========================
+
+const videoBtn = document.querySelector('#video-story-btn')
+const videoBtnIcon = document.querySelector('#video-story-btn-icon')
+const videoOverlay = document.querySelector('#video-story-overlay')
+const videoFile = document.querySelector('#video-story')
+videoBtn.addEventListener('click', function () {
+	function toggleOverlay(e) {
+		if (e.type === 'mouseleave') {
+			videoOverlay.classList.add('hidden')
+		} else {
+			videoOverlay.classList.remove('hidden')
+		}
+	}
+	if (videoFile.paused) {
+		videoFile.play()
+		videoBtnIcon.src = './img/music-player-pause-button-svgrepo-com.svg'
+		videoOverlay.onmouseleave = toggleOverlay
+		videoOverlay.onmouseenter = toggleOverlay
+	} else {
+		videoFile.paused()
+		videoBtnIcon.src = './img/story/play-white.svg'
+		videoOverlay.onmouseleave = null
+		videoOverlay.onmouseenter = null
+	}
+})
