9ebf69f29453ce4b87cd4011b6136f0fd1aa82d8
Ruslan Dunets <dunecruslan2@gmail.com>
2024-01-30 12:40:39 +0200

    added mobile menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const mobileNavBtn = document.querySelector('.mobile-nav-button')
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon')
+const mobileNav = document.querySelector('.mobile-nav')
+
+mobileNavBtn.addEventListener('click', function () {
+	mobileNavIcon.classList.toggle('active')
+	mobileNav.classList.toggle('active')
+	document.body.classList.toggle('no-scroll')
+})
